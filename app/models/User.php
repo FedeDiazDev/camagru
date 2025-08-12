8b081ebbee308e1e989c9f09099b881afe38fa7c
Federico Jose Diaz Guanoluiza <fdiaz-gu@c1r6s1.42madrid.com>
2025-08-12 17:29:29 +0200

    feat: User class

--- app/models/User.php
+++ app/models/User.php
@@ -1,20 +1,15 @@
 <?php
-//   findByUsername($username)
-//      findByEmail($email)
+
 //      verifyUser($email, $password)
-//      updateUser($id, $data)
-//      deleteUser($id)
 class User
 {
     private $connection;
 
-    public function __construct($db)
-    {
+    public function __construct($db){
         $this->connection = $db;
     }
 
-    public function getUserById($id)
-	{
+    public function getUserById($id){
 		$query = "SELECT * FROM user WHERE id = :id";
 		$stmt = $this->connection->prepare($query);
 		$stmt->bindParam(':id', $id, PDO::PARAM_INT);
@@ -23,8 +18,24 @@ class User
 		return $stmt->fetch(PDO::FETCH_OBJ);
 	}
 
-    public function createUser($username, $email, $password)
-    {
+    public function getUserByUsername($username){
+        $query = "SELECT * FROM user WHERE username = :username";
+        $stmt = $this->connection->prepare($query);
+        $stmt->bindParam(':username', $username);
+        $stmt->execute();
+
+        return $stmt->fetch(PDO::FETCH_OBJ);
+    }
+
+    public function getUserByEmail($mail){
+        $query = "SELECT * FROM user WHERE email :email";
+        $stmt = $this->connection->prepare($query);
+        $stmt->bindParam(':username', $username);
+
+        return $stmt->fetch(PDO::FETCH_OBJ);
+    }
+
+    public function createUser($username, $email, $password){
         $token = bin2hex(random_bytes(50));
         $hashed_pass = password_hash($password, PASSWORD_DEFAULT);
         $query = "INSERT INTO user (username, email, password, cofirmationToken) VALUES (:username, :email, :password, :token)";
@@ -34,6 +45,32 @@ class User
         $stmt->bindParam(':password', $hashed_pass);
         $stmt->bindParam(':token', $token);
 
+        if ($stmt->execute()) {
+            return true;
+        }
+        return false;
+    }
+
+    public function updateUser($id, $data){
+        $query = "UPDATE user set username = :username, email = :email, password = :password, emailPreference = :emailPreference WHERE id = :id";
+        $hashed_pass = password_hash($password, PASSWORD_DEFAULT);
+        $stmt = $this->connection->prepare($query);
+        $stmt->bindParam(':username', $username);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':password', $hashed_pass);
+
+        if ($stmt->execute()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function deleteUser($id)
+    {
+        $query = "DELETE FROM user WHERE id = :id";
+        $stmt = $this->connection->prepare($query);
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
         if ($stmt->execute()) {
             return true;
         }
